4a6d46931bae03b0280dba49500c877f65469463
Dababy110 <vargasariasdavid110@gmail.com>
2024-08-16 10:15:22 -0600

    Final Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Error404Page from './pages/Error/Error404Page.jsx';
-import HomePage from './pages/Home/HomePage.jsx';
+import Error404Page from "./pages/Error/Error404Page.jsx";
+import HomePage from "./pages/Home/HomePage.jsx";
 
 const App = () => {
-
   return (
-      <div className="bg-black h-screen flex justify-center items-center flex-col">
-        <h1 className="text-white text-[5.2em] lg:text-[7em] font-SairaBold tracking-widest">NEXUS</h1>
-        <p className="text-white text-[1.5em]">
-          Coming Soon
-        </p>
-      </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="*" element={<Error404Page />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
